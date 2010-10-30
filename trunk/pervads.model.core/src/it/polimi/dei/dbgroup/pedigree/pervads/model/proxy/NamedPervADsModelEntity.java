81f8158e7b320202e0e4821835815427f955e9da
usrlorecarra <usrlorecarra@503f4af4-0e9e-6bd7-51a0-2f919e806bf0>
2010-10-30 19:15:48 +0000

    Corrected a bug in localized property values retrieval

---
+++ trunk/pervads.model.core/src/it/polimi/dei/dbgroup/pedigree/pervads/model/proxy/NamedPervADsModelEntity.java
@@ -0,0 +1,8 @@
+package it.polimi.dei.dbgroup.pedigree.pervads.model.proxy;
+
+import it.polimi.dei.dbgroup.pedigree.contextmodel.proxy.NamedEntity;
+
+public interface NamedPervADsModelEntity extends PervADsModelEntity,
+		NamedEntity {
+
+}
--- trunk/pervads.model.core/src/it/polimi/dei/dbgroup/pedigree/pervads/model/proxy/OfferedItem.java
+++ trunk/pervads.model.core/src/it/polimi/dei/dbgroup/pedigree/pervads/model/proxy/OfferedItem.java
@@ -1,10 +1,8 @@
 package it.polimi.dei.dbgroup.pedigree.pervads.model.proxy;
 
-import it.polimi.dei.dbgroup.pedigree.contextmodel.proxy.NamedEntity;
-
 import com.hp.hpl.jena.rdf.model.Resource;
 
-public interface OfferedItem extends PervADsModelEntity, NamedEntity {
+public interface OfferedItem extends NamedPervADsModelEntity {
 	public Resource getOfferedItemIndividual();
 	public Offer getOffer();
 }
